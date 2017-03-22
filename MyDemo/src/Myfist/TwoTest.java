daa6061ff3adad8a378fc33e567bf6325baa792b
lovemomforever12 <644397946@qq.com>
2017-03-22 22:44:58 +0800

    Signed-off-by: lovemomforever12 <644397946@qq.com>

---
+++ MyDemo/src/Myfist/TwoTest.java
@@ -0,0 +1,17 @@
+package Myfist;
+
+public class TwoTest {
+	public static void main(String[] args) {
+
+		System.out.println("aaaa");
+
+		System.out.println("f");
+		
+		
+		System.out.println("");
+
+
+		System.out.println("tttt3432423ttttttttdfsafdas");
+
+	}
+}
